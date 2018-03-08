4ce7bec7f5a7fa8a3741f9da77854e878b10ab18
Charles Amat <charlie.amat@force5solutions.com>
2018-03-08 15:10:08 -0500

    Added embed content

--- infallible-code-discord-bot/Program.cs
+++ infallible-code-discord-bot/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Linq;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 using Discord;
 using Discord.Rest;
 using Discord.WebSocket;
@@ -29,43 +29,45 @@ namespace InfallibleCode.DiscordBot
 
             _client = new DiscordSocketClient();
             _client.Log += Log;
+            _client.Ready += ClientReady;
 
             await _client.LoginAsync(TokenType.Bot, discordToken);
             await _client.StartAsync();
 
-            UpdateRulesInterval();
-
             await Task.Delay(-1);
         }
 
-        public async void UpdateRulesInterval()
+        private Task ClientReady()
+        {
+            return Task.Run(() => UpdateRulesIntervalAsync());
+        }
+
+        public async void UpdateRulesIntervalAsync()
         {
             while(true)
             {
                 var rulesChannelId = ulong.Parse(GetConfigurationValue("RULES_CHANNEL_ID"));
-                var rulesMessageId = ulong.Parse(GetConfigurationValue("RULES_MESSAGE_ID")); 
+                var rulesMessageId = ulong.Parse(GetConfigurationValue("RULES_MESSAGE_ID"));
+                var embed = GenerateRulesEmbed();
 
                 var channel = _client.GetChannel(rulesChannelId) as SocketTextChannel;
                 if (channel == null)
                 {
                     Console.WriteLine($"Unable to locate channel (id:{rulesChannelId})");
                     break;
-                } else
+                }
+                else
                 {
-                    string action;
                     var message = await channel.GetMessageAsync(rulesMessageId);
                     if (message == null)
                     {
-                        action = "Posted";
-                        message = await channel.SendMessageAsync(action);
+                        message = await channel.SendMessageAsync(string.Empty, embed: embed);
                     } else
                     {
-                        action = "Updated";
-                        await ((RestUserMessage) message).ModifyAsync(x => x.Content = action);
+                        await ((RestUserMessage) message).ModifyAsync(x => x.Embed = embed);
                     }
-                    Console.WriteLine($"{action} rules message (id:{rulesMessageId}) in {channel.Name} (id:{message.Id})");
                 }
-                await Task.Delay(TimeSpan.FromSeconds(DefaultUpdateInterval));
+                await Task.Delay(TimeSpan.FromSeconds(DefaultUpdateInterval)).ConfigureAwait(false);
             }
         }
 
@@ -74,10 +76,50 @@ namespace InfallibleCode.DiscordBot
             var value = _configuration[key];
             if (string.IsNullOrWhiteSpace(value))
             {
-                throw new Exception($"Please enter a value for `{DefaultEnvironmentVariablePrex}{key}` in your environment variable.");
+                throw new Exception($"Please add `{DefaultEnvironmentVariablePrex}{key}` to your environment variables.");
             }
             return value;
         }
+
+        public Embed GenerateRulesEmbed() =>
+            new EmbedBuilder
+            {
+                Color = Color.Green,
+                Title = "Welcome to the Infallible Code Discord!",
+                Description = "We're a community of Unity developers helping each other become better programmers.",
+                Fields = new List<EmbedFieldBuilder>
+                {
+                    new EmbedFieldBuilder
+                    {
+                        Name = "Rules",
+                        IsInline = true,
+                        Value = "**1.** Respect everyone. Be polite.\n"
+                        + "**2.** No text/reaction spam.\n"
+                        + "**3.** No ALL CAPS messages.\n"
+                        + "**4.** No text walls. Use Pastepin.\n"
+                        + "**5.** No gore/porn/racism"
+                    },
+                    new EmbedFieldBuilder
+                    {
+                        Name = "Have a Question?",
+                        IsInline = true,
+                        Value = "• Post in the appropriate channel\n"
+                        + "• Be specific. Provide code (Gist/Pastebin)\n"
+                        + "• No begging. We'll do our best to help you.\n"
+                        + "• Search before posting."
+                    },
+                    new EmbedFieldBuilder
+                    {
+                        Name = "Links",
+                        IsInline = false,
+                        Value = "YouTube: https://youtube.com/infalliblecode\n"
+                        + "Twitch: http://twitch.tv/infalliblecode\n"
+                        + "Patreon: http://patreon.com/infalliblecode\n"
+                        + "Twitter: http://twitter.com/infalliblecode\n"
+                        + "Facebook: http://facebook.com/infalliblecode\n"
+                    },
+                }
+            }.Build();
 
         public Task Log(LogMessage message)
         {
